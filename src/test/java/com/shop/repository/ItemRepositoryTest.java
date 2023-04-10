f92eaa88ecfc81439dd1c5d41c33614ab04cafd9
mskim <hourses@naver.com>
2023-04-10 22:49:31 +0900

    쇼핑몰만들기

---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품저장 테스트")
+    public void createItemTest(){
+        Item item = new Item();
+
+        item.setItemNm("test Product");
+        item.setPrice(1000000);
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setItemDetail("테스트상품 설명");
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+
+
+
+
+    }
+
+}
